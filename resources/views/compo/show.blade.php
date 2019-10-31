412969e9a2d407c30387496fa719789c5f555519
velfox <timedevos@gmail.com>
2019-10-31 11:15:15 +0100

    style change

--- resources/views/compo/show.blade.php
+++ resources/views/compo/show.blade.php
@@ -1,5 +1,17 @@
 @extends('compo.layout')
 
+@php  $jup = "inschrijfen"  @endphp
+@foreach ($compo->summoner as $result)
+    @if ($result->user_id ==  Auth::user()->id )
+        @php $jup = "uitschrijfen"  @endphp
+        @break
+    @else
+        @php
+        $jup = "inschrijfen"
+        @endphp
+    @endif
+@endforeach
+
 @section('content')
     <div class="container">
         <div class="jumbotron">
@@ -8,13 +20,52 @@
             <hr class="my-4">
             <div class="row">
                     <div class="col-sm">
-                            <p>max players: <span class="badge badge-light">{{ $compo->maxplayers }}</span></p>
+
+
+
+
+                            <div class="card" style="width: 18rem;">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Deelnemers</h5>
+                                      @php $deelnemers = count($compo->summoner); @endphp
+                                      <p class="card-text">Aantal deelnemers <span class="badge badge-light">{{ $deelnemers }}</span> </p>
+                                      <hr class="my-4">
+                                      <div class="row">
+                                            <div class="col-sm">
+                                                <p>max: <span class="badge badge-light">{{ $compo->maxplayers }}</span></p>
+                                            </div>
+                                            <div class="col-sm">
+                                                <p>min: <span class="badge badge-light">{{ $compo->minplayers }}</span></p>
+                                            </div>
+                                      </div>
+                                    <hr class="my-4">
+                                        @if ($jup == "inschrijfen")
+                                            <form action="/summoner" method="post">
+                                                {{ csrf_field() }}
+                                                <input type="hidden" name="competition_id" value="{{$compo->id}}">
+                                            <button type="submit" class="btn btn-primary btn-lg" href="#" role="button">{{ $jup }}</button>
+                                            </form>
+                                        @else
+                                            <form action="/summoner/{{$compo->id}}" method="post">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+                                                <input type="hidden" name="competition_id" value="{{$compo->id}}">
+                                                <button type="submit" class="btn btn-danger btn-lg">uitschrijfen</button>
+                                            </form>
+                                        @endif
+                                    </div>
+                                  </div>
+
+                    </div>
+
+
+
+
+                    <div class="col-sm">
+
                     </div>
                     <div class="col-sm">
-                            <p>min players: <span class="badge badge-light">{{ $compo->minplayers }}</span></p>
-                    </div>
-                    <div class="col-sm">
-                            <p>min players: <span class="badge badge-light">{{ $compo->minplayers }}</span></p>
+
                     </div>
                   </div>
             <hr class="my-4">
@@ -38,38 +89,12 @@
             @endif
 
 
-            @php $test = count($compo->summoner);  @endphp
 
-                  <h1> {{$test}} </h1>
-            @php  $jup = "inschrijfen"  @endphp
-            @foreach ($compo->summoner as $result)
-                @if ($result->user_id ==  Auth::user()->id )
-                    @php $jup = "uitschrijfen"  @endphp
-                    @break
-                @else
-                    @php
-                    $jup = "inschrijfen"
-                    @endphp
-                @endif
-            @endforeach
 
             <hr class="my-4">
-            @if ($jup == "inschrijfen")
-            <form action="/summoner" method="post">
-                {{ csrf_field() }}
-                <input type="hidden" name="competition_id" value="{{$compo->id}}">
-            <button type="submit" class="btn btn-primary btn-lg" href="#" role="button">{{ $jup }}</button>
-            </form>
-            @else
-            <form action="/summoner/{{$compo->id}}" method="post">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-                <input type="hidden" name="competition_id" value="{{$compo->id}}">
-                <button type="submit" class="btn btn-danger btn-lg">uitschrijfen</button>
-            </form>
-            @endif
+
                 <a class="btn btn-primary btn-lg" href="/compo/{{ $compo->id }}/edit" role="button">bewerken</a>
-            <a class="btn btn-primary btn-lg" href="/compo" role="button">terug</a>
+                <a class="btn btn-primary btn-lg" href="/compo" role="button">terug</a>
         </div>
     </div>
 
